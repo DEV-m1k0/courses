589473bd4e863a4277eb7b9c5b4ae27a77771429
Артём Коваленко <artem@Artems-MacBook-Air.local>
2025-03-03 23:35:46 +0300

    Важные изменения

--- setup_and_run.ps1
+++ setup_and_run.ps1
@@ -1,17 +1,17 @@
-python -m venv .venv
+# python -m venv .venv
 
-$activateScript = ".\.venv\Scripts\Activate.ps1"
-if (Test-Path $activateScript) {
-    & $activateScript
-} else {
-    Write-Host "Не удалось найти скрипт активации виртуального окружения."
-    exit 1
-}
+# $activateScript = ".\.venv\Scripts\Activate.ps1"
+# if (Test-Path $activateScript) {
+#     & $activateScript
+# } else {
+#     Write-Host "Не удалось найти скрипт активации виртуального окружения."
+#     exit 1
+# }
 
-pip install -r requirements.txt
+# pip install -r requirements.txt
 
-python manage.py makemigrations
+# python manage.py makemigrations
 
-python manage.py migrate
+# python manage.py migrate
 
-python manage.py loaddata fixture/db.json
+# python manage.py loaddata fixture/db.json
